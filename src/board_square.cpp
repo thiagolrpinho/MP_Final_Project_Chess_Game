f510f61d6172b73cc0a8380d7007fe4ca757e562
Thiago Luis <thiagoluisrpinho@gmail.com>
2018-06-15 19:22:30 -0300

    Green for a board square cannot have a rank lower than 1

--- src/board_square.cpp
+++ src/board_square.cpp
@@ -7,9 +7,20 @@
 //! A constructor that creates a board square.
     /*!
         \Description Creates a new board square.
+        Both rank or file cannot be lower than 1 or
+        greater than 8.
+        
+        \param Two Int between (1 - 8)
     */
 BoardSquare::BoardSquare( int file_coordinate, int rank_coordinate )
-{
+{   
+    if ( rank_coordinate < 1 || rank_coordinate > 8)
+    {
+        throw 10;
+    } //if ( rank_coordinate < 1 || rank_coordinate > 8)
+
+    
+
     file_ = file_coordinate;
     rank_ = rank_coordinate;
 }
