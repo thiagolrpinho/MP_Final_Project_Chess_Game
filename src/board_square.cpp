3925a81e9e581c4351f4135144acf2260a069554
Thiago Luis <thiagoluisrpinho@gmail.com>
2018-06-15 12:54:57 -0300

    Created a source file for board_square class

---
+++ src/board_square.cpp
@@ -0,0 +1,13 @@
+#include "board_square.hpp"
+
+/*! \file board_square.cpp
+    \brief Module responsible to implement board square behaviors.
+*/
+
+//! A constructor that creates a board square.
+    /*!
+        \Description Creates a new board square.
+    */
+StringNode::StringNode(void)
+{
+}
