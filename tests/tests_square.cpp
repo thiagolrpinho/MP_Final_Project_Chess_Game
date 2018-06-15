11babed107e88f1f0e2da60b261dd2c4c8fdf12f
Thiago Luis <thiagoluisrpinho@gmail.com>
2018-06-15 12:46:28 -0300

    Created tests module for board squares

---
+++ tests/tests_square.cpp
@@ -0,0 +1,20 @@
+#include "square.hpp"
+
+//! These tests will be focused on board squares
+//! They'll be considered fully functional if they pass in
+//! four test cases: Create, Read, Update and Destroy
+
+TEST_CASE( "Create", "[square]" ) 
+{ 
+}
+
+TEST_CASE( "Read", "[square]" ) 
+{ 
+}
+
+TEST_CASE( "Update", "[square]" ) 
+{ 
+}
+TEST_CASE( "Destroy", "[square]" ) 
+{ 
+}
