6cfa3f07e2a3d1aacbd03e432aba230aec097426
Thiago Luis <thiagoluisrpinho@gmail.com>
2018-07-01 13:52:50 -0300

    Green for A board throws an error if wrong coordinates passed to read one of it's squares

--- src/board.cpp
+++ src/board.cpp
@@ -54,8 +54,8 @@ shared_ptr<Board> Board::getBoard(){
   */
   PBoardSquare Board::getBoardSquareAt( unsigned int i, unsigned int j  )
   {
-      if( i < 0 || i >= 8 ) return nullptr;
-      if( j < 0 || j >= 8 ) return nullptr;
+      if( i < 0 || i >= 8 ) throw (int) Error;
+      if( j < 0 || j >= 8 ) throw (int) Error;
       return _board_square_matrix[i][j];
   }
 
