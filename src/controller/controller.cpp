0e66b7fc7ed2e1b2b346745e44334b864e1bf17d
Gabriel Porto Oliveira <gabrielpoliveira@ig.com.br>
2018-07-02 23:42:41 -0300

    Created empty functions for controller
    Created empty functions for controller

---
+++ src/controller/controller.cpp
@@ -0,0 +1,40 @@
+
+
+char ** movepiece(char ** board){
+
+    //calls game engine and receive new board
+    return board;
+}
+
+void savegame(char ** board){
+
+    //calls savegame function
+
+}
+
+char ** loadboard(){
+
+    //calls load function
+    return board;
+
+}
+
+char ** showboards(char ** board){
+    //calls function for the boards
+    return board;
+}
+
+char ** newstandardgame(){
+    //calls function to create new standard board
+    return board;
+}
+
+char ** emptyboard(){
+    //calls function to create empty loadboard
+    return board;
+}
+
+char ** movepiecelocation(char ** board){
+    //calls function to create new board with the old one
+    return board;
+}
