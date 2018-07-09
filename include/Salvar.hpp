98fb04cac4da1a51b9a41088be109b51b58205a5
Axelmar <marcelo.acn@hotmail.com>
2018-07-08 23:18:10 -0300

    Adicionando modulo salvar

---
+++ include/Salvar.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include "../src/Salvar.cpp"
+
+void removepeca(char (*tabuleiro)[8][8], char peca);
+void carregaTab(char (*tabuleiro)[8][8]);
+void salvarTab(char (*tabuleiro)[8][8]);
