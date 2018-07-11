516102e36a794f74ffdb35ff3b4d3b0e164f0965
Gabriel Porto Oliveira <gabrielpoliveira@ig.com.br>
2018-07-10 22:07:06 -0300

    I don't know what I'm doing with my life anymore

---
+++ src/ai.cpp
@@ -0,0 +1,26 @@
+
+
+
+char ai(char board[8][8]){
+    int cont=0;
+    do{
+        //generates play
+        if(){//if opponent piece is destroyed
+
+            cont = 9;
+        }
+
+        cont++;
+    }while(x!=9);
+    if(x == 9){
+        //generates random play
+        return board;
+
+    }else{
+
+        return board;
+
+    }
+    return board;
+
+}
