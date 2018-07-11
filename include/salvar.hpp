62bc44adeda69f3a969c1aa18de817903cdf71fc
Axelmar <marcelo.acn@hotmail.com>
2018-07-10 22:25:06 -0300

    Add files via upload

--- include/salvar.hpp
+++ include/salvar.hpp
@@ -1,14 +1,10 @@
-#ifndef SALVAR_HPP
-#define SALVAR_HPP
-
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include "../src/salvar.cpp"
 
-void removepeca(char (*tabuleiro)[8][8], char peca);
+void removepeca(char (*tabuleiro)[8][8], char peca, int x, int y);
 void carregaTab(char (*tabuleiro)[8][8]);
 void salvarTab(char (*tabuleiro)[8][8]);
+bool verificar(char (*tabuleiro)[8][8], char peca);
 void editar(char (*tabuleiro)[8][8]);
-
-
-#endif
